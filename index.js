10b87dddeb269d47bebc1217d68b4737134b7f9e
Min Cho <minsangcho19@gmail.com>
2021-12-17 12:15:33 -0800

    commit tree f873c0a48356cfab5e8ff64d2afa9c23db5918f3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ref = document.getElementById("text");
+    ref.textContent = "This is really cool!";
+  });
+  
+  
